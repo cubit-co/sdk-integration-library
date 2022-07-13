8d92dfd948003333abb1dc141d0d29d9eab2f99d
Andres Calvo Ariza <andres.calvo@cubit.com.co>
2022-07-13 10:45:32 -0500

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import { TAucoSDK, Config } from './types';
+
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
+
+export const AucoSDK: TAucoSDK = params => {
+  parametersValidation(params);
+  setupEvents(params);
+};
+
+AucoSDK({
+  iframeId: 'myIframe',
+  keyPublic: '',
+  language: 'es',
+  sdkType: 'upload',
+  events: {
+    onSDKClose: () => {},
+    onSDKReady: () => {},
+  },
+  sdkData: {
+    userAttributes: {
+      email: '',
+      firstName: '',
+      lastName: '',
+    },
+  },
+});
+const parametersValidation = (params: Config) => {
+  if(!params.iframeId){
+    throw new Error("Could not start SDK, iframeID is missing");
+  }
+  if (!['es', 'en'].includes(params?.language)) {
+    throw new Error(
+      "Could not start SDK, language is missing or invalid, available options are 'es' and 'en' "
+    );
+  }
+  // const validCountry =
+  //   params?.country.length > 1 &&
+  //   params?.country.length <= 3 &&
+  //   new RegExp(/\+[0-9]{1,3}/).test(params?.country);
+  // if (!validCountry) {
+  //   throw new Error('Could not start SDK, invalid country, country is the country dial code, for example +57 for Colombia');
+  // }
+};
+const setupEvents = (params: Config) => {
+  const { iframeId, events, language, sdkData } = params;
+  const origin = '';
+  const iframe = document.getElementById(iframeId) as HTMLIFrameElement;
+  if (!iframe) {
+    throw new Error(
+      `Could not start SDK, Iframe with id: ${iframeId} not found`
+    );
+  }
+  function onMessage(event: MessageEvent) {
+    if (event.origin != origin) return;
+    if (event.data.ready) {
+      iframe!.contentWindow?.postMessage({ language, ...sdkData }, origin);
+      return;
+    }
+    if (event.data.type.includes('token')) {
+      console.log('Recibí evento token del sdk', event);
+      if (!events.onSDKToken) {
+        throw new Error(
+          "Could not get token, SDK is asking for user token, but there isn't a onSDKToken function provided"
+        );
+      }
+      const token = events.onSDKToken();
+      iframe!.contentWindow?.postMessage({ type: 'token', token }, origin);
+    }
+    if (event.data.type == 'SDK-CLOSE') {
+      events.onSDKClose();
+      window.removeEventListener('message', onMessage);
+    }
+    if (event.data) {
+      console.log('Respuesta final del iframe', event);
+    }
+  }
+  window.addEventListener('message', onMessage);
+  iframe.src = origin;
+};
---
+++ src/types.ts
@@ -0,0 +1,64 @@
+export type Languages = 'es' | 'en';
+export type SDKs = 'upload' | 'attachments' | 'validation' | 'sign';
+export type SDKEvents = {
+  onSDKReady: () => void;
+  onSDKClose: () => void;
+  onSDKToken?: () => string;
+};
+interface BaseConfig {
+  keyPublic?: string;
+  country?: string;
+  iframeId: string;
+  language: Languages;
+  events: SDKEvents;
+}
+interface SDKUploadData {
+  userAttributes: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone?: string;
+    identification?: string;
+    identificationType?: string;
+  };
+}
+interface SDKUpload extends BaseConfig {
+  /**
+   * Create an upload flow with signers and approvers
+   */
+  sdkType: 'upload';
+  sdkData: SDKUploadData;
+}
+interface SDKAttachments extends BaseConfig {
+  /**
+   * Create an attachment flow with signers. You could not add approvers.
+   *  In this flow there is only one approver, that is the person who creates it.
+   */
+  sdkType: 'attachments';
+  sdkData: SDKUploadData;
+}
+interface SDKSign extends BaseConfig {
+  /**
+   * Sign flow, use this flow to let your users sign your documents
+   */
+  sdkType: 'sign';
+  sdkData: {
+    image: string;
+    name: string;
+    uxOptions: {
+      primaryColor: string;
+      redirectUrl: string;
+    };
+    document: string;
+  };
+}
+interface SDKValidation extends BaseConfig {
+  /**
+   * Create a validation flow, Your customer validate his face against his
+   * ID Photo and you receive a response with the similarity between those two photos
+   */
+  sdkType: 'validation';
+  sdkData: {};
+}
+export type Config = SDKUpload | SDKAttachments | SDKSign | SDKValidation;
+export type TAucoSDK = (params: Config) => void;
